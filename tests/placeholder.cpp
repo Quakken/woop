47652e67c0588c13e6a193c2971d67a205b869c6
Quakken <69021181+Quakken@users.noreply.github.com>
2025-11-05 15:17:39 -0800

    Migrating to new testing framework
    Moved source files to woop_core and set up tests.

--- tests/placeholder.cpp
+++
@@ -1,5 +0,0 @@
-#include "gtest/gtest.h"
-
-TEST(Placeholder, Test1) {
-  ASSERT_TRUE(true);
-}
---
+++ tests/wad.cpp
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+#include "wad.hpp"
+
+TEST(Wads, Open) {}
+
+TEST(Wads, Close) {}
+
+TEST(Wads, GetLump) {}
