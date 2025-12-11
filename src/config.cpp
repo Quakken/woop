7a3bed217cbcc86ca004a5ae0f3fcda6dec3e0c7
Quakken <69021181+Quakken@users.noreply.github.com>
2025-12-11 14:28:18 -0800

    Comments and headers
    Added function headers to config.hpp. Fixed a bug where woop exceptions would not store error messages (leading to an invalid access exception when read).

--- src/config.cpp
+++ src/config.cpp
@@ -1,14 +1,41 @@
+/**
+ * @file config.hpp
+ * @authors quak
+ * @brief Defines functions declared in config.hpp
+ */
+
 #include "config.hpp"
 #include "level.hpp"
+#include "renderer.hpp"
+#include "wad.hpp"
 
 namespace config {
+glm::ivec2 get_array_as_ivec2(const toml::array& value) {
+  return glm::ivec2{
+      value.at(0).as_integer()->get(),
+      value.at(1).as_integer()->get(),
+  };
+}
+woop::Pixel get_array_as_pixel(const toml::array& value) {
+  return woop::Pixel{
+      value.at(0).as_integer()->get(),
+      value.at(1).as_integer()->get(),
+      value.at(2).as_integer()->get(),
+      255,
+  };
+}
+
 woop::Wad get_wad(const toml::table& table) {
   std::optional<std::string> wad_path =
       table["general"]["wad"].value<std::string>();
   if (!wad_path)
     throw ConfigException(
         "No WAD given. Specify a WAD to load with \"general.wad\"");
-  return woop::Wad{*wad_path};
+  try {
+    return woop::Wad{*wad_path};
+  } catch (woop::Exception& exception) {
+    throw ConfigException(exception.what());
+  }
 }
 woop::Level get_level(const woop::Wad& wad, const toml::table& table) {
   std::optional<std::string> level_name =
@@ -16,92 +43,111 @@ woop::Level get_level(const woop::Wad& wad, const toml::table& table) {
   if (!level_name)
     throw ConfigException(
         "No level given. Specify a level with \"general.level\"");
-  return woop::Level{wad, *level_name};
+  try {
+    return woop::Level{wad, *level_name};
+  } catch (woop::Exception& exception) {
+    throw ConfigException(exception.what());
+  }
 }
 
 woop::WindowConfig get_window_config(const toml::table& table) {
   woop::WindowConfig cfg;
+  // Title
   if (const auto& entry = table["window"]["title"].value<std::string>())
     cfg.title = entry.value();
+  // Resolution
   if (const auto* entry = table["window"]["resolution"].as_array()) {
-    // Resolution needs to be read in two parts
-    cfg.resolution.x = entry->at(0).as_integer()->get();
-    cfg.resolution.y = entry->at(1).as_integer()->get();
+    try {
+      cfg.resolution = get_array_as_ivec2(*entry);
+    } catch (std::exception& exception) {
+      throw ConfigException(
+          "Invalid value given to \"window.resolution\" (expected [width, "
+          "height])");
+    }
   }
+  // Fullscreen
   if (const auto& entry = table["window"]["fullscreen"].value<bool>())
     cfg.fullscreen = entry.value();
+  // Resizable
   if (const auto& entry = table["window"]["resizable"].value<bool>())
     cfg.resizable = entry.value();
+  // Decorated
   if (const auto& entry = table["window"]["decorated"].value<bool>())
     cfg.decorated = entry.value();
   return cfg;
 }
 woop::CameraConfig get_camera_config(const toml::table& table) {
   woop::CameraConfig cfg;
+  // Near plane
   if (const auto& entry = table["camera"]["near_plane"].value<double>())
     cfg.near_plane = entry.value();
+  // Far plane
   if (const auto& entry = table["camera"]["far_plane"].value<double>())
     cfg.far_plane = entry.value();
+  // FOV
   if (const auto& entry = table["camera"]["fov"].value<double>())
     cfg.fov = entry.value();
   return cfg;
 }
 woop::RendererConfig get_renderer_config(const toml::table& table) {
   woop::RendererConfig cfg;
+  // Vertex shader
   if (const auto& entry =
           table["renderer"]["vertex_shader"].value<std::string>())
     cfg.shaders.vert_path = entry.value();
+  // Fragment shader
   if (const auto& entry =
           table["renderer"]["fragment_shader"].value<std::string>())
     cfg.shaders.frag_path = entry.value();
+  // Clear color
   if (const auto* entry = table["renderer"]["clear_color"].as_array()) {
     try {
-      cfg.clear_color.r =
-          static_cast<std::byte>(entry->at(0).as_integer()->get());
-      cfg.clear_color.g =
-          static_cast<std::byte>(entry->at(1).as_integer()->get());
-      cfg.clear_color.b =
-          static_cast<std::byte>(entry->at(2).as_integer()->get());
+      cfg.clear_color = get_array_as_pixel(*entry);
     } catch (std::exception& except) {
       throw ConfigException(
-          "Invalid value given to \"renderer.clear_color\" (expected array of "
-          "3 integers)");
+          "Invalid value given to \"renderer.clear_color\" (expected [r, g, "
+          "b])");
     }
   }
+  // Fog color
   if (const auto* entry = table["renderer"]["fog_color"].as_array()) {
     try {
-      cfg.fog_color.r =
-          static_cast<std::byte>(entry->at(0).as_integer()->get());
-      cfg.fog_color.g =
-          static_cast<std::byte>(entry->at(1).as_integer()->get());
-      cfg.fog_color.b =
-          static_cast<std::byte>(entry->at(2).as_integer()->get());
+      cfg.fog_color = get_array_as_pixel(*entry);
     } catch (std::exception& except) {
       throw ConfigException(
-          "Invalid value given to \"renderer.fog_color\" (expected array of "
-          "3 integers)");
+          "Invalid value given to \"renderer.fog_color\" (expected [r, g, "
+          "b])");
     }
   }
+  // Fog strength
   if (const auto& entry = table["renderer"]["fog_strength"].value<double>())
     cfg.fog_strength = entry.value();
   return cfg;
 }
 woop::PlayerConfig get_player_config(const toml::table& table) {
   woop::PlayerConfig cfg;
+  // Height
   if (const auto& entry = table["player"]["height"].value<double>())
     cfg.camera_height = entry.value();
+  // Gravity
   if (const auto& entry = table["player"]["gravity"].value<double>())
     cfg.gravity = entry.value();
+  // Sensitivity
   if (const auto& entry = table["player"]["sensitivity"].value<double>())
     cfg.sensitivity = entry.value();
+  // Move speed
   if (const auto& entry = table["player"]["move_speed"].value<double>())
     cfg.move_speed = entry.value();
+  // Acceleration
   if (const auto& entry = table["player"]["acceleration"].value<double>())
     cfg.acceleration = entry.value();
+  // Drag
   if (const auto& entry = table["player"]["drag"].value<double>())
     cfg.drag = entry.value();
+  // Mouse rotation
   if (const auto& entry = table["player"]["enable_mouse"].value<bool>())
     cfg.enable_mouse = entry.value();
+  // Flight
   if (const auto& entry = table["player"]["enable_flight"].value<bool>())
     cfg.enable_flight = entry.value();
   return cfg;
--- src/config.hpp
+++ src/config.hpp
@@ -1,3 +1,10 @@
+/**
+ * @file config.hpp
+ * @authors quak
+ * @brief Declares a handful of functions to generate Woop configurations from
+ * a toml file.
+ */
+
 #pragma once
 
 #include "window.hpp"      /* woop::Window */
@@ -10,16 +17,42 @@
 #include "toml++/toml.hpp" /* Configuration parsing */
 
 namespace config {
+/**
+ * @brief Exception thrown when a configuration-related error occurs.
+ */
 struct ConfigException : public woop::Exception {
  public:
   ConfigException(const std::string_view& what) : woop::Exception(what) {}
 };
 
+/**
+ * @brief Returns the wad specified in the renderer's configuration file.
+ */
 woop::Wad get_wad(const toml::table& table);
+/**
+ * @brief Returns the level of the wad specified in the renderer's configuration
+ * file.
+ */
 woop::Level get_level(const woop::Wad& wad, const toml::table& table);
 
+/**
+ * @brief Fills a window configuration with values present in a configuration
+ * file.
+ */
 woop::WindowConfig get_window_config(const toml::table& table);
+/**
+ * @brief Fills a camera configuration with values present in a configuration
+ * file.
+ */
 woop::CameraConfig get_camera_config(const toml::table& table);
+/**
+ * @brief Fills a renderer configuration with values present in a configuration
+ * file.
+ */
 woop::RendererConfig get_renderer_config(const toml::table& table);
+/**
+ * @brief Fills a player configuration with values present in a configuration
+ * file.
+ */
 woop::PlayerConfig get_player_config(const toml::table& table);
 }  // namespace config
